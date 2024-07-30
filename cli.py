7dac26b35bd129e2ec51625ed3db9f80978e3cc9
PARTHIB DEB <parthibkumardeb@gmail.com>
2024-07-31 00:20:54 +0530

    add : Specific Comments in cli.py

--- cli.py
+++ cli.py
@@ -5,8 +5,12 @@ import click
 @click.command()
 @click.option("-t", help="enter the text")
 def commandline(t):
-    if ".txt" in t:
-        with open(t, "r") as input:
+    """
+    Takes either a (.txt) file or a textual paragraph as argument
+    """
+    if ".txt" in t:  # Check whether .txt is in the argument
+
+        with open(t, "r") as input:  # Open the .txt file to read in texual mode
             response = ollama.chat(
                 model="qwen2:0.5b",
                 messages=[
@@ -16,23 +20,26 @@ def commandline(t):
                     },
                 ],
             )
-            res = bytes(response["message"]["content"], "utf-8")
+            res = bytes(response["message"]["content"], "utf-8")  # Convert the textual string into binary string or array of bytes
             input.close()
-        with open("summary.txt", "wb") as output:
+
+        with open("summary.txt", "wb") as output:  # Open the summary.txt file in write mode for binary strings
             output.write(res)
             output.close()
     else:
+
         response = ollama.chat(
             model="qwen2:0.5b",
             messages=[
                 {
                     "role": "user",
-                    "content": f"Summarize the content of (in lesser words) - {t}",
+                    "content": f"Summarize the content of (in lesser words) - {t}",  # Send the raw textual argument to Model
                 },
             ],
         )
+
         res = response["message"]["content"]
-        print(f"\n\n\t{res}\t\n\n")
+        print(f"\n\n\t{res}\t\n\n")  # Paste the textual output in shell
 
 
 if __name__ == "__main__":
